793f7c291ed1a21d9d2939a5c570e2bc7702a907
“Vishnu <vvichugit@gmail.com>
2018-09-11 23:20:10 -0500

    added some changes

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -1,6 +1,18 @@
 class Api::TasksController < ApplicationController
 
 
+
+
+
+  def inda
+   
+   @tas = Task.all 
+
+   render json:{message:"Mark Zuckerberg is a computer Programmer"}
+
+  end 
+
+
   def index 
 
     @task = Task.all 
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
  namespace :api do 
 
+   root "tas" => "tasks#inda"
+
     get "tas" => "tasks#index"
 
  end 
